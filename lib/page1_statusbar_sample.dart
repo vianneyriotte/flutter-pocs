b0944217f0a690515519728f8c4620cd847d9c7d
Vianney RIOTTE <vianney.riotte@gmail.com>
2021-05-02 19:09:06 +0200

    Test CachedNetworkImage

--- lib/page1_statusbar_sample.dart
+++ lib/page1_statusbar_sample.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pocs/components/effects/effects.dart';
 import 'package:flutter_pocs/components/pages/page_base.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Page1 extends StatefulWidget {
   Page1({Key key}) : super(key: key);
@@ -22,21 +23,22 @@ class _Page1State extends State<Page1> {
       ),
       child: Stack(
         children: [
-          PageBase.scrollableView(
-            context,
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: List.generate(
-                  nb,
-                  (index) => ListTile(
-                    title: Text("Titre ${index.toString()}"),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          _listCachedNetworkImage3(),
+          // PageBase.scrollableView(
+          //   context,
+          //   Padding(
+          //     padding: EdgeInsets.all(10),
+          //     child: Column(
+          //       children: List.generate(
+          //         nb,
+          //         (index) => ListTile(
+          //           title: Text("Titre ${index.toString()}"),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // _listNetworkImage(),
+          _listCachedNetworkImage(),
           Positioned(
             bottom: 20,
             right: 20,
@@ -54,7 +56,41 @@ class _Page1State extends State<Page1> {
     );
   }
 
-  Widget _listCachedNetworkImage3() {
+  Widget _listCachedNetworkImage() {
+    return PageBase.scrollableView(
+      context,
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: List.generate(
+            this.nb,
+            (index) => Container(
+              key: GlobalKey(),
+              child: CachedNetworkImage(
+                imageBuilder: (context, imageProvider) => FadeIn(
+                  child: Container(
+                    height: 150,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) => CircularProgressIndicator(),
+                imageUrl: 'https://picsum.photos/250?image=9',
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _listNetworkImage() {
     return PageBase.scrollableView(
       context,
       Padding(
